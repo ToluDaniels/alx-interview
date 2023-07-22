87b36b013af041ae4599a0a561397e65c056e494
Tolu Daniels <93607903+ToluDaniels@users.noreply.github.com>
2023-07-22 09:27:45 +0100

    updated

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,54 +1,45 @@
 #!/usr/bin/python3
 
 import sys
+import signal
 
-
-def print_msg(dict_sc, total_file_size):
-    """
-    Method to print
-    Args:
-        dict_sc: dict of status codes
-        total_file_size: total of the file
-    Returns:
-        Nothing
-    """
-
-    print("File size: {}".format(total_file_size))
-    for key, val in sorted(dict_sc.items()):
-        if val != 0:
-            print("{}: {}".format(key, val))
-
-
+# Initialize variables to store metrics
 total_file_size = 0
-code = 0
-counter = 0
-dict_sc = {"200": 0,
-           "301": 0,
-           "400": 0,
-           "401": 0,
-           "403": 0,
-           "404": 0,
-           "405": 0,
-           "500": 0}
+status_code_counts = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+line_counter = 0
 
-try:
-    for line in sys.stdin:
-        parsed_line = line.split()  # ✄ trimming
-        parsed_line = parsed_line[::-1]  # inverting
+def print_statistics():
+    global total_file_size, status_code_counts
+    print("File size:", total_file_size)
+    for code, count in sorted(status_code_counts.items()):
+        if count > 0:
+            print(f"{code}: {count}")
 
-        if len(parsed_line) > 2:
-            counter += 1
+# Define a signal handler for CTRL + C
+def signal_handler(sig, frame):
+    print_statistics()
+    sys.exit(0)
 
-            if counter <= 10:
-                total_file_size += int(parsed_line[0])  # file size
-                code = parsed_line[1]  # status code
+# Set the signal handler for CTRL + C
+signal.signal(signal.SIGINT, signal_handler)
 
-                if (code in dict_sc.keys()):
-                    dict_sc[code] += 1
+# Process input from stdin line by line
+for line in sys.stdin:
+    line_counter += 1
+    try:
+        # Parse the line using the input format
+        ip_address, _, _, _, _, status_code, file_size = line.split()[0:7]
+        status_code = int(status_code)
+        file_size = int(file_size)
 
-            if (counter == 10):
-                print_msg(dict_sc, total_file_size)
-                counter = 0
+        # Update metrics
+        total_file_size += file_size
+        status_code_counts[status_code] += 1
 
-finally:
-    print_msg(dict_sc, total_file_size)
+        # Print statistics after every 10 lines
+        if line_counter % 10 == 0:
+            print_statistics()
+
+    except ValueError:
+        # If the line doesn't match the expected format, skip it
+        continue
